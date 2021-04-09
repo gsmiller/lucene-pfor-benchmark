4ea67a313850c6fbe8c73390ba5970a12644e71b
Greg Miller <gmiller@amazon.com>
2021-04-08 17:35:12 -0700

    fix back to use BLOCK_SIZE

--- src/main/java/gsmiller/PForCandidateDecoder.java
+++ src/main/java/gsmiller/PForCandidateDecoder.java
@@ -10,10 +10,10 @@ class PForCandidateDecoder extends AbstractPForDecoder {
     @Override
     protected void prefixSumOf(long[] longs, long base, long val) {
         System.arraycopy(IDENTITY_PLUS_ONE, 0, longs, 0, ForUtil.BLOCK_SIZE);
-        for (int i = 0; i < longs.length; ++i) {
+        for (int i = 0; i < ForUtil.BLOCK_SIZE; ++i) {
             longs[i] *= val;
         }
-        for (int i = 0; i < longs.length; ++i) {
+        for (int i = 0; i < ForUtil.BLOCK_SIZE; ++i) {
             longs[i] += base;
         }
     }
